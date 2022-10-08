cc4e4c8ca5f3d51b129444772387cbfd3b2740dd
Ilham Nopi Hendri <Ilhamnopihendri@gmail.com>
2022-10-08 16:23:11 +0700

    test-reactjs-07

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 // import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from "@tabler/icons";
-import { useState } from "react";
+
 import Button from "./components/Button";
-import Card from "./components/Card";
+import Counter from "./components/Counter";
 import PlaceContentCenter from "./components/PlaceContentCenter";
 function App() {
-  const [counter, setcount] = useState(0);
-
-  function handleClick() {
-    setcount((prevState) => prevState + 1);
-  }
   return (
     <PlaceContentCenter>
-      <h1 className="text-5xl font-bold">{counter}</h1>
-      <div className="mt-5 flex justify-center gap-2">
-        <Button onClick={handleClick}>+1</Button>
-        <Button
-          onClick={() => {
-            handleClick();
-            handleClick();
-            handleClick();
-          }}
-        >
-          +3
-        </Button>
-      </div>
+      <Counter initialValue={10}></Counter>
     </PlaceContentCenter>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+import { useState } from "react";
+export default function Counter({ initialValue }) {
+  const [count, setcount] = useState(initialValue);
+  function handleClick() {
+    setcount((prevState) => prevState + 1);
+  }
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">{count}</h1>
+      <div className="mt-5 flex justify-center gap-2">
+        <Button onClick={handleClick}>+1</Button>
+        <Button
+          onClick={() => {
+            handleClick();
+            handleClick();
+            handleClick();
+          }}
+        >
+          +3
+        </Button>
+      </div>
+    </div>
+  );
+}
