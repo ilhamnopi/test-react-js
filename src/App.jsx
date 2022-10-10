ec604744f79db51adde9da5ba2b91b17655ce004
Ilham Nopi Hendri <Ilhamnopihendri@gmail.com>
2022-10-10 15:54:32 +0700

    test-reactjs-09

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,7 @@
-import { IconBrandGithub } from "@tabler/icons";
-import Label from "./components/Label";
-import Input from "./components/input";
-import Button from "./components/Button";
-import Card from "./components/Card";
 import PlaceContentCenter from "./components/PlaceContentCenter";
-import { useState } from "react";
-
+import Todo from "./components/Todo";
 function App() {
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-  });
-  const onChange = (event) => {
-    setForm({ ...form, [event.target.name]: event.target.value });
-  };
-
-  const submit = (event) => {
-    event.preventDefault();
-    console.log(form);
-  };
-  return (
-    <PlaceContentCenter>
-      <form action="" onSubmit={submit}>
-        <Card>
-          <Card.Title>Sign up for new account!</Card.Title>
-          <Card.Body>
-            <div className="mb-5 border border-slate-400 rounded-lg  p-4">
-              <p>Name : {form.name || "-------"}</p>
-              <p>Email : {form.email || "-------"}</p>
-            </div>
-            <div className="mb-6">
-              <Label htmlFor="name" value={"Name"}>
-                Name
-              </Label>
-              <Input id={"name"} name={"name"} value={form.name} onChange={onChange} />
-            </div>
-            <div className="mb-6">
-              <Label htmlFor="email" value={"Email"}>
-                Email
-              </Label>
-              <Input type="email" id={"email"} name={"email"} value={form.email} onChange={onChange} />
-            </div>
-          </Card.Body>
-          <Card.Footer>
-            <Button>
-              <IconBrandGithub></IconBrandGithub> Register
-            </Button>
-          </Card.Footer>
-        </Card>
-      </form>
-    </PlaceContentCenter>
-  );
+  return <Todo></Todo>;
 }
 
 export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,80 @@
+import Card from "./Card";
+import PlaceContentCenter from "./PlaceContentCenter";
+import Input from "./input";
+import Button from "./Button";
+import { useState } from "react";
+import { IconFileX } from "@tabler/icons";
+
+export default function Todo() {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTask] = useState([]);
+
+  function handleAddTask(e) {
+    e.preventDefault();
+
+    setTask((prevTask) => [
+      ...prevTask,
+      {
+        id: Math.floor(Math.random() * Date.now()),
+        name: newTask,
+        completed: false,
+      },
+    ]);
+    setNewTask("");
+  }
+  function handleCompleteTask(id) {
+    const updateTask = tasks.map((task) => {
+      console.log(id);
+      if (id === task.id) {
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      }
+      return task;
+    });
+    setTask(updateTask);
+  }
+  function handleRemoveTask(id) {
+    const removeTask = tasks.filter((task) => task.id !== id);
+    setTask(removeTask);
+    console.log(id);
+  }
+
+  return (
+    <PlaceContentCenter>
+      <Card>
+        <Card.Title>Todo Apps</Card.Title>
+        <Card.Body>
+          <form action="">
+            <div className="flex items-center gap-x-2">
+              <Input value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+              <Button text={"Add Task"} onClick={handleAddTask} />
+            </div>
+          </form>
+          {tasks.length > 0 ? (
+            <ol className="space-y-2 mt-4">
+              {tasks.map((task) => (
+                <li key={task.id} className={"flex items-center justify-between"}>
+                  <span>
+                    {task.name} {task.completed ? "✔" : "❌"}
+                  </span>
+                  <div className="flex items-center gap-x-2">
+                    <button className={"px-2 py-1 border text-xs"} onClick={() => handleCompleteTask(task.id)}>
+                      Mark as {task.completed ? "completed" : "Incompleted"}
+                    </button>
+                    <button className={"px-2 py-1 border text-xs"} onClick={() => handleRemoveTask(task.id)}>
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          ) : null}
+        </Card.Body>
+
+        <Card.Footer>you have {tasks.length} Task</Card.Footer>
+      </Card>
+    </PlaceContentCenter>
+  );
+}
